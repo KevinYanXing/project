5bc150d7c3c4419283c4482e5f5d8c6cf7b790a2
kevin <347252351@qq.com>
2017-03-21 19:15:45 +0800

    开始表单操作

--- app/forms/mainform.py
+++ app/forms/mainform.py
@@ -1,10 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField,DateField
+from wtforms import StringField, SubmitField,DateField,PasswordField
 from fsuper import SDateField
 from wtforms.validators import Required
+from flask import redirect,url_for
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     birthday = SDateField('when is your birthday?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class SignupForm(FlaskForm):
+    n = StringField('Input your your name:', validators=[Required()])
+    # b = SDateField('Input your your birthday:', validators=[Required()])
+    # m= SDateField('Input your your mobile phone:', validators=[Required()])
+    # p = PasswordField('Input your password:', validators=[Required()])
+    # rp = PasswordField('Repeat your password:', validators=[Required()])
+
+    submit = SubmitField('Submit')
+
+    def validate_n(self, field):
+        print field.data
+        raise ValueError,'You may input a fake date =.=!'
+
+    def validate_on_submit(self):
+        print 1111111
+        return redirect(url_for('frontend.index'))
+
+
+
--- app/views/frontend.py
+++ app/views/frontend.py
@@ -1,17 +1,17 @@
 # coding=utf-8
 from flask import render_template, session, redirect, url_for, flash,Blueprint
-from app.forms.mainform import NameForm
+from app.forms.mainform import NameForm,SignupForm
 from mdb.kndb.main import Kn
 from time import mktime,strptime,localtime,strftime
 frontend = Blueprint('frontend', __name__)
 
 @frontend.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    try:
+    birth = ''
+    if session.get('birth'):
         birth = strftime('%Y-%m-%d',localtime(session.get('birth')))
-    except:
-        birth = ''
+
+    form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
@@ -20,4 +20,13 @@ def index():
         session['birth'] = form.birthday.data
         return redirect(url_for('frontend.index'))
 
-    return render_template('frontend/index.html', form=form, name=session.get('name'),birth=birth)
+    return render_template('frontend/index.html', form=form, name=session.get('name'),birth = birth)
+
+@frontend.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        print form.n.data,'00000000'
+    if form.is_submitted():
+        return form.validate_on_submit()
+    return render_template('frontend/signup.html', form=form)
