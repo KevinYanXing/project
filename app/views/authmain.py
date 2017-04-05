425c02eca607c2d2cc1362bdbd81b0f9b12ff882
kevin <347252351@qq.com>
2017-04-05 10:32:49 +0800

    个人资料页面初步

--- app/views/authmain.py
+++ app/views/authmain.py
@@ -1,10 +1,11 @@
-from flask import Blueprint,send_from_directory,abort,redirect,url_for
+from flask import Blueprint,send_from_directory,abort,redirect,url_for,render_template
 import os
-from flask_login import logout_user
+from flask_login import logout_user,login_required
 
 authmain = Blueprint('authmain', __name__)
 
 @authmain.route('/upload/<string:pho>/', methods=['GET', 'POST'])
+@login_required
 def upload(pho):
     if not pho:
         return abort(404)
@@ -16,6 +17,12 @@ def upload(pho):
     return send_from_directory(pth, filename, add_etags=False, cache_timeout=2592000)
 
 @authmain.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('frontend.index'))
+
+@authmain.route('/personal/info')
+@login_required
+def personal_info():
+    return render_template('authmain/personal_info.html')
